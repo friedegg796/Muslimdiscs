3258a3e906d4919d55be73185e1ea03dff6534f9
Hlodr2 <ohi10@outlook.com>
2026-05-03 18:46:37 -0400

    Second item tab made for Nasheed discs, but in all the first disc has been finished :sob:

--- src/main/java/net/awanderingmuslim/muslimdiscs/item/ModItemGroups.java
+++ src/main/java/net/awanderingmuslim/muslimdiscs/item/ModItemGroups.java
@@ -10,10 +10,21 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroups {
-    public static final ItemGroup PINK_GARNET_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
-            Identifier.of(Muslimdiscs.MOD_ID, "pink_garnet_items"),
+    public static final ItemGroup MUSLIMDISCS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(Muslimdiscs.MOD_ID, "muslimdisc_discs"),
             FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.EID_TAKBIR_MUSIC_DISC))
                     .displayName(Text.translatable("itemgroup.muslimdiscs.muslimdiscs"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModItems.EID_TAKBIR_MUSIC_DISC);
+
+
+                    }).build());
+
+
+    public static final ItemGroup MUSLIMDISCS_NASHEED_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(Muslimdiscs.MOD_ID, "muslimdisc_discs_nasheeds"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.EID_TAKBIR_MUSIC_DISC))
+                    .displayName(Text.translatable("itemgroup.muslimdiscs.muslimdiscs_nasheed"))
                     .entries((displayContext, entries) -> {
                         entries.add(ModItems.EID_TAKBIR_MUSIC_DISC);
 
